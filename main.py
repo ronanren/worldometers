65f47f44558034a2fba27322203a98c5a52f0266
ronanren <renouxpro@gmail.com>
2020-09-20 19:38:03 +0200

    update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def home():
     return "<h1>Unofficial Worldometers.info API</h1><p>This site is a prototype API for get data from Worldometers.info</p>"
 
 
-@app.route('/api/coronavirus/', methods=['GET'])
+@app.route('/api/coronavirus/all/', methods=['GET'])
 def api_all():
 
     data = []
@@ -35,7 +35,33 @@ def api_all():
             data.append(dict(zip(key, re.split('\n', tr.text[1:])[:-2])))
         i += 1
 
-    return jsonify(data)
+    res = dict()
+    res['data'] = data
+    return res
+
+
+@app.route('/api/coronavirus/country/<country>', methods=['GET'])
+def api_country(country):
+
+    data = []
+    key = ['place', "Country", "Total Cases", "New Cases", "Total Deaths", "New Deaths", "Total Recovered",
+           "New Recovered", "Active Cases", "Critical", "Total Cases/1M pop", "Deaths/1M pop", "Total Tests", "Tests/1M pop", "Population", "Region"]
+    req = urllib.request.Request('https://www.worldometers.info/coronavirus/',
+                                 headers={'User-Agent': 'Mozilla/5.0'})
+    source = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(source, 'html.parser')
+    tables = soup.find_all('tbody')
+    table_rows = tables[0].find_all('tr')
+    i = 0
+    for tr in table_rows:
+        if i > 6:
+            if (dict(zip(key, re.split('\n', tr.text[1:])[:-2]))['Country'].lower() == country.lower()):
+                data.append(dict(zip(key, re.split('\n', tr.text[1:])[:-2])))
+        i += 1
+
+    res = dict()
+    res['data'] = data
+    return res
 
 
 app.run(debug=True)
